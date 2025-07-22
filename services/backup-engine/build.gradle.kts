b2b08c548440bd1dccf1191b76fae0bc144c9292
Wiktor <wiktator.olszewski@gmail.com>
2025-07-23 01:30:48 +0200

    fix(build): add gradle wrapper and correct npm ci command

--- services/backup-engine/build.gradle.kts
+++ services/backup-engine/build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "3.1.5"
     id("io.spring.dependency-management") version "1.1.3"
-    kotlin("jvm") version "1.8.22"
+    kotlin("jvm")
     kotlin("plugin.spring") version "1.8.22"
 }
 
@@ -14,9 +14,6 @@ java {
     sourceCompatibility = JavaVersion.VERSION_17
 }
 
-repositories {
-    mavenCentral()
-}
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
