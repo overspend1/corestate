2a174d77d1c8c6d817112f3cdf500c8448d2ec15
Wiktor <overspend1@users.noreply.github.com>
2025-07-23 03:04:44 +0200

    fix(build): correct Dagger Hilt plugin configuration
    - Add com.google.dagger.hilt.android plugin to root build.gradle.kts
    - Update androidApp build.gradle.kts to use correct plugin ID
    - Resolves workflow failures due to missing plugin definitions
    
    Made by Wiktor/overspend1

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@
 plugins {
     id("com.android.application") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("com.google.dagger.hilt.android") version "2.48" apply false
     id("org.springframework.boot") version "3.1.5" apply false
     id("io.spring.dependency-management") version "1.1.3" apply false
 }
