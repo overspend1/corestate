cf28e038d84de0adad020a1210c557cc578504f3
Wiktor <overspend1@users.noreply.github.com>
2025-07-23 03:12:30 +0200

    fix(build): add missing Gradle plugins to root configuration
    - Add kotlin.multiplatform plugin
    - Add kotlin.plugin.serialization plugin
    - Add com.android.library plugin
    - Add com.google.protobuf plugin
    
    Made by Wiktor/overspend1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,12 @@
 // CoreState-v2/build.gradle.kts
 plugins {
     id("com.android.application") version "8.2.0" apply false
+    id("com.android.library") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("org.jetbrains.kotlin.multiplatform") version "1.9.0" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.0" apply false
     id("com.google.dagger.hilt.android") version "2.48" apply false
+    id("com.google.protobuf") version "0.9.4" apply false
     id("org.springframework.boot") version "3.1.5" apply false
     id("io.spring.dependency-management") version "1.1.3" apply false
 }
